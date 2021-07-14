11ea4e50dcf78b8714ca72081fe8656cbddda689
Cameron Min <51976632+keyongjun@users.noreply.github.com>
2021-07-15 00:14:47 +0900

    Update print_schedule.py
    수료식까지 남은 날 추가, 파일 디렉토리 없을 경우 다운로드 -> 구현 중

--- print_schedule.py
+++ print_schedule.py
@@ -1,34 +1,43 @@
 import pandas as pd
 from datetime import datetime
-#import urllib.request
-#import os
-#from string import ascii_lowercase
+import os
+from urllib import request
+from pathlib import Path
+import gdown
+ 
+url = 'https://drive.google.com/file/d/1bMP5JUjnCyQCG9-t_OiWfLdy2Msk8n_e/view'
+output = '인공지능을 활용한 웹 서비스 개발자 양성과정 (12기)_커리큘럼.xlsx'
+gdown.download(url, output, quiet=False)
 
 #cmd 내 필요한 커맨드 키 
 #pip install pandas
 #pip install xrld
-#pip install beautifulsoup4
+#pip install beautifulsoup4                   
 
+
+
+    
 def print_schedule():
-    #drive_location = input("사용하는 드라이브 명을 기입해주세요 [예: C, D, E, F, ... ]")
-    # alpha_list = list(ascii_lowercase) -> ['a', 'b', 'c', .... 'z']
-    #while (drive_location.lower() not in alpha_list):
-        #print("올바른 드라이브 명을 입력하세요: ")
-        #drive_location = input()
     
-    #drive_location을 이용해서 import os로 파일 찾는 구현
     
-    # if 파일이 없음 경우 -> 파일 다운로드 구현
-        # url = "https://www.notion.so/5d13426d3675461c953a8663d5670f40#56f1d5cd162b445890cc389a6d9b7e3a";
-        # saved_file_name = "인공지능을 활용한 웹 서비스 개발자 양성과정 (12기)_커리큘럼.xlsx"
-        # urllib.request.urlretrieve(url, saved_file_name)
-    
-    df = pd.read_excel('C:/Users/USER/Downloads/인공지능을 활용한 웹 서비스 개발자 양성과정 (12기)_커리큘럼.xlsx')
-    now = datetime.now()
-    now = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
-    for i in range(len(df['일자'])):
-        if (now == df['일자'][i]):
-            print('오늘자 플레이데이터 교육 일수는 {0}일차로, 배울 과목 주제는 {1}이고, 해당 과목 세부 주제는 {2} 입니다.'.format(df['일수'][i], df['주제'][i], df['세부'][i]))
-            break;
-    
+    downloads_path = str(Path.home() / "Downloads") + "\\" + output
+    print(downloads_path)
+    schedule = pd.read_excel(downloads_path)
+    now = datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+
+
+    END_DATE = datetime(2021, 12, 27, 18, 0, 0)
+    수료식 = END_DATE - datetime.now()
+
+    for i in range(len(schedule)):
+        일자 = schedule.values[i][0]
+        일수 = schedule.values[i][1]
+        주제 = schedule.values[i][2]
+        세부 = schedule.values[i][3]
+        if (now == 일자):
+            print("오늘은 {0}입니다.".format(now))
+            print('오늘의 플레이데이터 교육 일수는 {0}일차로, 이번 주차 과목 주제는 {1}이고, 해당 과목 세부 주제는 {2} 입니다.'.format(일수, 주제, 세부))
+            break
+
+    print('수료까지', 수료식.days,'일 남았습니다')
 print_schedule()
