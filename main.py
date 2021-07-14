9b3737a92b503786891493f6665ad16f1fdc810b
geesuee <baejisu97@gmail.com>
2021-07-14 15:34:06 +0900

    main.py 수정 및 test.py 생성

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
+
 import weather as wt
 import corona as cn
 
 
 def startChat():
-    global answer  # answer를 전역변수로 설정
+    global answer
     answer = input("채팅을 시작하시겠습니까?  Y or N\n")
 
     if answer == "Y":
@@ -12,13 +13,14 @@ def startChat():
         print("안녕하세요. 채팅을 시작합니다.")
         time.sleep(1)
 
-        global name  # 이름을 전역변수로 설정
+        global name
         name = input("이름을 입력해주세요.\n")
 
     elif answer == "N":
         endChat()
 
     else:
+        time.sleep(1)
         print("Y 혹은 N로 대답해주세요.\n")
         time.sleep(1)
         startChat()
@@ -31,36 +33,50 @@ def endChat():
 
 
 def choose():
+    time.sleep(1)
     print()
     print(name + "님, 무엇을 도와드릴까요?")
     time.sleep(1)
-    print("1:날씨 정보 \t 2:플레이데이터 시간표 \t 3:점심메뉴 고르기 \t 4:미니게임 \t 5: 코로나 확진 현황 \t 999: 챗봇 종료")
+
+    global cnum
+    print("실행하고 싶은 메뉴 번호를 입력해주세요.")
     time.sleep(1)
 
-    cnum = input("실행하고 싶은 메뉴 번호를 입력해주세요.\n")
+    cnum = input(
+        "1:날씨 정보 \t 2:플레이데이터 시간표 \t 3:점심메뉴 고르기 \t 4:미니게임 \t 5: 코로나 확진 현황 \t 999: 챗봇 종료\n")
 
-    if cnum == "1":
-        time.sleep(1)
-        wt.weather()
-    elif cnum == "2":
-        print("\n시간표 시작")
-        # timetable()
-    elif cnum == "3":
-        print("\n점심메뉴 시작")
-        # lunch()
-    elif cnum == "4":
-        print("\n미니게임 시작")
-        # minigame()
-    elif cnum == "5":
-        time.sleep(1)
-        cn.corona()
-    elif cnum == "999":
-        endChat()
-    else:
-        print("\n잘못 입력하셨습니다. 다시 입력해주세요.")
+    clist = ["1", "2", "3", "4", "5", "999"]
+    if cnum not in clist:
+        print("메뉴 번호를 잘못 입력하셨습니다.")
         time.sleep(1)
         choose()
 
 
-startChat()
-choose()
+def func():
+    if cnum in ["1", "2", "3", "4", "5"]:
+
+        if cnum == "1":
+            time.sleep(1)
+            wt.weather()
+        elif cnum == "2":
+            print("\n시간표 시작")
+            # timetable()
+        elif cnum == "3":
+            print("\n점심메뉴 시작")
+            # lunch()
+        elif cnum == "4":
+            print("\n미니게임 시작")
+            # minigame()
+        elif cnum == "5":
+            time.sleep(1)
+            cn.corona()
+
+        print()
+        time.sleep(1)
+        print("----------------------------------------"*3)
+        time.sleep(1)
+        choose()
+        func()
+
+    else:
+        endChat()
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import *
+
+startChat()
+choose()
+func()
