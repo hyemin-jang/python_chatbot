64b557c3d04e5486c5c3d972cf9d8c0af5fd0237
Cameron Min <51976632+keyongjun@users.noreply.github.com>
2021-07-15 12:44:14 +0900

    Update print_schedule.py
    플레이데이터 링크 수정 및 불필요한 라이브러리 제거

--- print_schedule.py
+++ print_schedule.py
@@ -2,7 +2,6 @@ import pandas as pd
 from datetime import datetime
 import os
 import urllib.request
-from pathlib import Path
 import time
 
 
@@ -12,7 +11,6 @@ import time
 # pip install os
 # pip install datetime
 # pip install requests
-# pip install pathlib
 
 
 url = "https://blog.kakaocdn.net/dn/b1lTSN/btq9zFt84zP/RxpfvHb8bQCqzcuXHIYKf0/%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5%EC%9D%84%2B%ED%99%9C%EC%9A%A9%ED%95%9C%2B%EC%9B%B9%2B%EC%84%9C%EB%B9%84%EC%8A%A4%2B%EA%B0%9C%EB%B0%9C%EC%9E%90%2B%EC%96%91%EC%84%B1%EA%B3%BC%EC%A0%95%2B%2812%EA%B8%B0%29_%EC%BB%A4%EB%A6%AC%ED%81%98%EB%9F%BC.xlsx?attach=1&knm=tfile.xlsx"
@@ -42,4 +40,3 @@ def print_schedule():
             break
     time.sleep(1)
     print('수료까지', 수료식.days, '일 남았습니다')
-print_schedule()
