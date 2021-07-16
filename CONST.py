4bc79063e5bb4875fd089786fd998f40d16a7704
Cameron Min <51976632+keyongjun@users.noreply.github.com>
2021-07-16 11:23:21 +0900

    Add files via upload
    END_DATE, URL, PLAYDATA_FILE 등 고정값이 필요한 파일들은 새로운 파이썬 파일 내에 라이브러리화 시킴

---
+++ CONST.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+class CONST(object):
+    __slots__ = ()
+    URL = "https://blog.kakaocdn.net/dn/b1lTSN/btq9zFt84zP/RxpfvHb8bQCqzcuXHIYKf0/%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5%EC%9D%84%2B%ED%99%9C%EC%9A%A9%ED%95%9C%2B%EC%9B%B9%2B%EC%84%9C%EB%B9%84%EC%8A%A4%2B%EA%B0%9C%EB%B0%9C%EC%9E%90%2B%EC%96%91%EC%84%B1%EA%B3%BC%EC%A0%95%2B%2812%EA%B8%B0%29_%EC%BB%A4%EB%A6%AC%ED%81%98%EB%9F%BC.xlsx?attach=1&knm=tfile.xlsx"
+    PLAYDATA_FILE = "인공지능을 활용한 웹 서비스 개발자 양성과정 (12기)_커리큘럼.xlsx"
+    END_DATE = datetime(2021,12,27, 18, 0, 0)
